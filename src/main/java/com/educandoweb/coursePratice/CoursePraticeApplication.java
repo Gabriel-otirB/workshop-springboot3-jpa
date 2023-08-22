5ca944cbafa35a5186b6a8e9a8e333b1a57a8328
Gabriel otirB <lordglayder@gmail.com>
2023-08-22 09:35:29 -0300

    Project created

---
+++ src/main/java/com/educandoweb/coursePratice/CoursePraticeApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursePratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePraticeApplication.class, args);
+	}
+
+}
