2f11a67310e9a99dec86c55dcf046841cc6de2f4
Gabriel otirB <lordglayder@gmail.com>
2023-08-29 08:44:42 -0300

    Many-to-many association

--- src/main/java/com/educandoweb/coursePratice/entities/Category.java
+++ src/main/java/com/educandoweb/coursePratice/entities/Category.java
@@ -5,12 +5,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -22,7 +24,8 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	Set<Product> products = new HashSet<>();
 	
 	public Category() {
@@ -51,6 +54,10 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
