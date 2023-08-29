b18e06840e23e1777ddca49ef10bef9ef08d7e6e
Gabriel otirB <lordglayder@gmail.com>
2023-08-29 08:32:20 -0300

    Product

---
+++ src/main/java/com/educandoweb/coursePratice/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.coursePratice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.coursePratice.entities.Product;
+import com.educandoweb.coursePratice.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
